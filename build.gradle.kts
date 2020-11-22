d915f91605fa017b33b53a8609fa5dec1a08c030
kazumoto <motch0214@gmail.com>
2020-11-22 16:18:23 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.eighthours"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
