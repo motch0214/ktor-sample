dc99d7b0e60dce43bdd700b603a9181572454c83
kazumoto <motch0214@gmail.com>
2020-11-22 20:24:42 +0900

    chore: Enable to run with docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     kotlin("jvm") version "1.4.10"
+    id("com.google.cloud.tools.jib") version "2.6.0"
 }
 
 allprojects {
@@ -19,6 +20,29 @@ dependencies {
     implementation(project(":app"))
 }
 
+val mainClass = "com.eighthours.sample.app.ApplicationKt"
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Main-Class"] = mainClass
+    }
+}
+
+jib {
+    to.image = "${project.name}:latest"
+}
+tasks.jib {
+    doFirst {
+        val image = findProperty("jib.image") as String?
+        if (image != null) {
+            jib {
+                to.image = "$image:$version"
+                to.tags = emptySet()
+            }
+        }
+    }
+}
+
 application {
-    mainClassName = "com.eighthours.sample.app.ApplicationKt"
+    mainClassName = mainClass
 }
